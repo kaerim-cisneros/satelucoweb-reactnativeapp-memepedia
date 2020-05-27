f6e0043777ec254ebcdf5a5d5cbad9f19e86cf23
kaerim-cisneros <kaerim.cisneros@gmail.com>
2020-05-27 06:41:51 -0600

    adding router and second screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import router from "./utils/router";
+import { createAppContainer} from "react-navigation";
+const AppContainer = createAppContainer(router);
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <AppContainer />
     </View>
   );
 }
@@ -12,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
