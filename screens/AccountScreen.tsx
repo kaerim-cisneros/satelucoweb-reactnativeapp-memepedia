96cc641bad77b6abae22bf426bfaa1630bb96ddd
kaerim-cisneros <kaerim.cisneros@gmail.com>
2020-05-27 23:54:32 -0600

    add 2 new screens Account and PostForm

---
+++ screens/AccountScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+
+
+export default () => {
+  return (
+    <View>
+      <Text> 
+          Cuantas cuentas
+        </Text>
+    </View>
+  );
+};
---
+++ screens/PostFormScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+
+
+export default () => {
+  return (
+    <View>
+      <Text> 
+          Like a Sunflower
+        </Text>
+    </View>
+  );
+};
