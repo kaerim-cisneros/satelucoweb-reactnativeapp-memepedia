c0c5449d976879e0317b75493d7c1f78f5b1b318
kaerim-cisneros <kaerim.cisneros@gmail.com>
2020-05-28 22:26:41 -0600

    import BottomTab Bar to its own file also adding some style

---
+++ components/navigation/BottomTabBar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+   container: {
+       backgroundColor: "#ff5a5f",
+       paddingTop: 15,
+       paddingBottom: 15,
+       marginTop:15
+   } 
+});
+
+interface IBottomTabBarProps{
+    navigate: (arg: string) => void;
+}
+
+export default (props: IBottomTabBarProps) => {
+    return <View style={styles.container}>
+        <TouchableOpacity onPress={() => props.navigate("Feed")}>
+            <Text>Feed</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => props.navigate("Search")}>
+            <Text>Search</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => props.navigate("PostForm")}>
+            <Text>Form</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => props.navigate("Account")}>
+            <Text>Account</Text>
+        </TouchableOpacity>
+    </View>
+}
