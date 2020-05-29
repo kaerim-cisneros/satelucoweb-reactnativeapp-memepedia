a0cf2addb10818e01b9e5a943263d4723b1f862c
kaerim-cisneros <kaerim.cisneros@gmail.com>
2020-05-28 23:12:58 -0600

    add more style Bottom Tab Bar

--- syles/navigation/bottomTabBar.ts
+++ syles/navigation/bottomTabBar.ts
@@ -4,10 +4,17 @@ import {primary} from "../colors";
 
 export default StyleSheet.create({
    container: {
-       backgroundColor: primary,
-       paddingTop: 15,
-       paddingBottom: 15,
-       marginTop:15,
-       color: "#fff"
+        backgroundColor: primary,
+        paddingTop: 15,
+        paddingBottom: 15,
+        marginTop:15,
+        position: "absolute",
+        bottom: 0,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        width: "100%",
+        height: 60
+               
    } 
 });
