9984fde65718240bb796512641189b9ed6337cf9
kaerim-cisneros <kaerim.cisneros@gmail.com>
2020-05-31 02:08:21 -0600

    Adding bottons and set up state on the texts

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, TouchableOpacity} from "react-native";
 
 export default () => {
+
+    const [formToShow, setFormToShow] = useState("LOGIN");
+
+    const sceenTypeText = () =>{
+        if (formToShow === "LOGIN") {
+            return "Need an account? Register";
+        } else if (formToShow === "REGISTER") {
+            return "Already have an account? Login"
+        }
+    };
+
+    const headerText = () =>{
+        if (formToShow === "LOGIN") {
+            return "Login";
+        } else if (formToShow === "REGISTER") {
+            return "Register"
+        }
+    };
+
+    const handleAuthTypePress =() => {
+        if (formToShow === "LOGIN") {
+           setFormToShow("REGISTER");
+        } else if (formToShow === "REGISTER") {
+            setFormToShow("LOGIN");
+        }
+    };
+
     return (
         <View style= {{ marginTop: 100}}>
-            <Text>Login</Text>
+            <Text>{headerText()}</Text>
 
-            <TouchableOpacity>
-                <Text>Already have an account? Register</Text>
+            <TouchableOpacity onPress={handleAuthTypePress}>
+                <Text>{sceenTypeText()}</Text>
             </TouchableOpacity>
         </View>
     );    
