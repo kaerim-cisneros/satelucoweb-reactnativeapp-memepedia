8af750c65cb339cccfedd8d1c232f6ec4a04e0cb
kaerim-cisneros <kaerim.cisneros@gmail.com>
2020-05-31 02:47:44 -0600

    adding style to auth screen

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import {View, Text, TouchableOpacity, TextInput} from "react-native";
 
+import {primary, dark} from "../../syles/colors";
+
 export default () => {
 
     const [formToShow, setFormToShow] = useState("LOGIN");
@@ -31,19 +33,30 @@ export default () => {
     };
 
     return (
-        <View style= {{ marginTop: 100}}>
-            <Text>{headerText()}</Text>
+        <View style= {{ marginTop: 100, backgroundColor: primary, height: "100%" }}>
+            <Text style={{ color: "white", fontWeight:"bold", paddingTop: 30, paddingLeft: 30}}>{headerText()}</Text>
 
             <View style={{ marginTop: 20, marginBottom: 20 }}>
                 <TextInput  
+
+                    style={{
+                        paddingLeft:30, 
+                        backgroundColor: "white", 
+                        borderRadius:20,
+                        height:40,
+                        color: dark
+                    }}
+                   
                     placeholder="Email"
                     value={email}
                     onChangeText={val => setEmail(val)}
+                    autoCapitalize="none"
+                    spellCheck = {false}
                 />
             </View>
 
             <TouchableOpacity onPress={handleAuthTypePress}>
-                <Text>{sceenTypeText()}</Text>
+                <Text style={{ paddingLeft:30, color: "white"}}>{sceenTypeText()}</Text>
             </TouchableOpacity>
         </View>
     );    
