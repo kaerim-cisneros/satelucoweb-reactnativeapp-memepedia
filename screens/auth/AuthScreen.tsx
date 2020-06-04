8b62a80a760c2f9bc52e81c891c572417f74906b
kaerim-cisneros <kaerim.cisneros@gmail.com>
2020-06-04 13:11:35 -0600

    add funcionallity to the submit button

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,13 +1,23 @@
 import React, {useState} from "react";
 import {View, Text, TouchableOpacity, TextInput} from "react-native";
 
+
 import {lightGrey} from "../../syles/colors";
 
 import textInputStyles from "../../syles/forms/textInputStyles"
 const { textFieldWrapper, inputField } = textInputStyles
 import authScreenStyles from "../../syles/stacks/auth/authScreenStyles"
 
-export default () => {
+import API from "../../utils/api";
+
+interface IAuthScreenProps {
+    navigation: {
+        navigate: (arg: string) => void;
+    };
+};
+
+
+export default (props: IAuthScreenProps) => {
 
     const [formToShow, setFormToShow] = useState("LOGIN");
     const [email, setEmail] = useState("");
@@ -27,6 +37,27 @@ export default () => {
         } else if (formToShow === "REGISTER") {
             return "Register"
         }
+    };
+
+    const handleSubmit = () => {
+        const userAuth = {
+            auth: {
+                email: email,
+                password: password
+            }
+        }
+    API
+        .post("memipedia_user_token", userAuth)
+        .then(response => {
+            console.log("Response from handle submit",response.data);
+            if (response.data.jwt) {
+                props.navigation.navigate("Feed")
+            }
+        })
+        .catch(error => {
+            console.log("error getting token", error);
+        });
+
     };
 
     const handleAuthTypePress =() => {
@@ -71,6 +102,10 @@ export default () => {
 
             <TouchableOpacity onPress={handleAuthTypePress}>
                 <Text style={{ color: lightGrey, paddingTop:10}}>{sceenTypeText()}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={handleSubmit}>
+                <Text style={{ color: "white", paddingTop:40}}>{headerText()}</Text>
             </TouchableOpacity>
         </View>
     );    
