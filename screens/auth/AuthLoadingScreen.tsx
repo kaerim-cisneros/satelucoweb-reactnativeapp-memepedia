d8601e55e8a157bd5e806375d6f7873497d993fc
kaerim-cisneros <kaerim.cisneros@gmail.com>
2020-06-24 23:43:00 -0600

    Building the Check Login API Call

--- screens/auth/AuthLoadingScreen.tsx
+++ screens/auth/AuthLoadingScreen.tsx
@@ -3,6 +3,7 @@ import { View } from "react-native";
 
 import * as SecureStore from "expo-secure-store";
 import CurrentUserContext from "../../utils/context/CurrentUserContext"
+import api from "../../utils/api";
 
 
 
@@ -23,7 +24,25 @@ export default (props: IAuthLoadingScreenProps) => {
         const token = await SecureStore.getItemAsync("mempedia_secure_token")
     
     if (token) {
+        api.get("logged_in", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            console.log("response from check login", response.data);
 
+            if (response.data.memipedia_user) {
+                setCurrentUser(response.data.memipedia_user);
+                props.navigation.navigate("App");
+            } else {
+                setCurrentUser(null);
+                props.navigation.navigate("Auth");
+            }
+        })
+        .catch((error) => {
+            setCurrentUser(null);
+            props.navigation.navigate("Auth");
+        })
     } else{
         setCurrentUser(null);
         props.navigation.navigate("Auth");
